ba095e5addee08a5ccac9a8b01bc44f3ff50c7ae
Bryce Hamrick <brycehamrick@gmail.com>
2012-04-12 15:02:04 -0700

    reverted

--- janrain_capture_oauth.tpl.php
+++ janrain_capture_oauth.tpl.php
@@ -1,22 +1,15 @@
 <?php global $base_url; ?>
-<?php global $user; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" >
   <body>
     <p>Please wait...</p>
-   <?php print $user->created; ?>
-   <?php 
-   if ((time() - $user->created) < 10) {
-     echo "I'm new!";
-   }
-   ?>
     <script type="text/javascript">
-      if (window.location.href != parent.window.location.href) {
-        if (parent.window.location.href.indexOf("logout") > 1) {
-          parent.window.location.href = "<?php echo $base_url; ?>";
+      if (window.location.href != window.parent.location.href) {
+        if (window.parent.location.href.indexOf("logout") > 1) {
+          window.parent.location.href = "<?php echo $base_url; ?>";
         } else {
-          parent.window.location.reload();
+          window.parent.location.reload();
         }
       } else {
         window.location.href = "<?php echo $base_url; ?>";

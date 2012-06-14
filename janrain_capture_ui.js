8cc60bb2e7b57871bcdb6a850c646d773da149d3
Bryce Hamrick <brycehamrick@gmail.com>
2012-06-13 17:05:48 -0700

    1621208 - Make Janrain UI handle all login/register links in core

--- janrain_capture_ui.js
+++ janrain_capture_ui.js
@@ -29,6 +29,19 @@
 
   Drupal.behaviors.janrainCaptureUi = {
     attach: function(context, settings) {
+
+      // Make all Capture signin and profile links appear in a fancybox.
+      var links = $('a.janrain_capture_anchor').once('capture-processed');
+      var length = links.length;
+      if (links.length !== 0) {
+        var i, link;
+        for (i = 0; i < length; i++) {
+          link = links[i];
+          // Add the necessary classes to ensure the link is opened in a dialog.
+          $(link).addClass('iframe fancy');
+        }
+      }
+
       $(".fancy").fancybox({
         padding: 0,
         scrolling: "no",

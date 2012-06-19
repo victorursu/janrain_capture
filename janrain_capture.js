8ae4066653ed44398b4fad35c8ea451f2ceb6c4c
Bryce Hamrick <brycehamrick@gmail.com>
2012-06-19 14:17:12 -0700

    1645580 - The "enforce" setting should add the janrain_capture_anchor class to all user/login and user/register links

--- janrain_capture.js
+++ janrain_capture.js
@@ -41,6 +41,18 @@ Drupal.janrainCapture.prototype = {
 
 Drupal.behaviors.janrainCapture = {
   attach: function(context, settings) {
+    if (settings.janrainCapture.enforce) {
+      // Modify all /user/login and /user/register links to use Capture.
+      var links = $('a[href^="/user/login"], a[href^="/user/register"]').once('janrain-capture');
+      var length = links.length;
+      if (links.length !== 0) {
+        var i, link;
+        for (i = 0; i < length; i++) {
+          link = links[i];
+          $(link).addClass('janrain_capture_anchor janrain_capture_signin');
+        }
+      }
+    }
     if (typeof(settings.janrainCapture.backplane_server) != 'undefined'
       && typeof(settings.janrainCapture.backplane_bus_name) != 'undefined') {
       Backplane(CAPTURE.bp_ready);
--- janrain_capture_ui.js
+++ janrain_capture_ui.js
@@ -1,5 +1,7 @@
 (function ($) {
 
+  Drupal.janrainCapture = Drupal.janrainCapture || {};
+
   // Override the resize method on the Drupal.janrainCapture object so
   // as to use a fancybox for it.
   Drupal.janrainCapture.resize = function(jargs) {
@@ -31,24 +33,15 @@
     attach: function(context, settings) {
 
       // Make all Capture signin and profile links appear in a fancybox.
-      var links = $('a.janrain_capture_anchor').once('capture-processed');
-      var length = links.length;
-      if (links.length !== 0) {
-        var i, link;
-        for (i = 0; i < length; i++) {
-          link = links[i];
-          // Add the necessary classes to ensure the link is opened in a dialog.
-          $(link).addClass('iframe fancy');
-        }
+      if ($.fn.fancybox) {
+        $(".janrain_capture_anchor", context).once("capture-ui").addClass('iframe').fancybox({
+          padding: 0,
+          scrolling: "no",
+          autoScale: true,
+          width: 666,
+          autoDimensions: false
+        });
       }
-
-      $(".fancy").fancybox({
-        padding: 0,
-        scrolling: "no",
-        autoScale: true,
-        width: 666,
-        autoDimensions: false
-      });
     }
   };
 })(jQuery);

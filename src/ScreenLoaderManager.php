2eb359dd31ee07e15f992edc6c0009ee92229838
Sergii Bondarenko <broken@firstvector.org>
2018-05-08 17:47:02 +0300

    Do not require the trailing slash in a path to Janrain screens

--- src/ScreenLoaderManager.php
+++ src/ScreenLoaderManager.php
@@ -19,7 +19,7 @@ class ScreenLoaderManager {
   const CACHE_DIR = 'public://janrain_capture_screens/cache';
 
   /**
-   * Allowed janrain screen names.
+   * Allowed Janrain screen names.
    *
    * @var string[]
    */
@@ -33,6 +33,8 @@ class ScreenLoaderManager {
 
   /**
    * Allowed Janrain screen types.
+   *
+   * @var string[]
    */
   const ALLOWED_TYPES = ['js', 'html'];
 
@@ -41,21 +43,21 @@ class ScreenLoaderManager {
    *
    * @var string
    */
-  private $path;
+  protected $path;
 
   /**
    * Logger.
    *
    * @var \Psr\Log\LoggerInterface
    */
-  private $logger;
+  protected $logger;
 
   /**
-   * Http client.
+   * HTTP client.
    *
    * @var \GuzzleHttp\Client
    */
-  private $httpClient;
+  protected $httpClient;
 
   /**
    * ScreenLoaderManager constructor.
@@ -68,9 +70,8 @@ class ScreenLoaderManager {
    *   HTTP client.
    */
   public function __construct(ConfigFactoryInterface $configFactory, LoggerChannelFactoryInterface $logger_factory, Client $client) {
-    $this->path = $configFactory->get('janrain_capture.settings')
-      ->get('screens.folder');
-    $this->logger = $logger_factory->get("janrain_capture");
+    $this->path = rtrim($configFactory->get('janrain_capture.settings')->get('screens.folder'), '/');
+    $this->logger = $logger_factory->get('janrain_capture');
     $this->httpClient = $client;
   }
 
@@ -81,7 +82,7 @@ class ScreenLoaderManager {
    *   TRUE when the Janrain screens path is remote.
    */
   public function isRemote() {
-    return (strpos($this->path, 'http') === 0);
+    return strpos($this->path, 'http') === 0;
   }
 
   /**
@@ -96,19 +97,20 @@ class ScreenLoaderManager {
    *   Janrain screen contents.
    */
   public function getScreen($name, $type) {
-    if (!(in_array($name, self::ALLOWED_SCREENS) && in_array($type, self::ALLOWED_TYPES))) {
+    if (!in_array($name, static::ALLOWED_SCREENS) || !in_array($type, static::ALLOWED_TYPES)) {
       return '';
     }
-    if (!$this->isRemote()) {
-      $file_name = $this->path . $name . '.' . $type;
-    }
-    else {
-      $file_name = sprintf('%s/%s.%s', self::CACHE_DIR, $name, $type);
-    }
+
+    $file_name = static::buildPath($this->isRemote() ? static::CACHE_DIR : $this->path, $name, $type);
+
     if (!is_readable($file_name)) {
-      $this->logger->error("Unable to read @filename", ['@filename' => $file_name]);
+      $this->logger->error('Unable to read @filename', [
+        '@filename' => $file_name,
+      ]);
+
       return '';
     }
+
     return file_get_contents($file_name);
   }
 
@@ -116,29 +118,54 @@ class ScreenLoaderManager {
    * Update Janrain screens cache if screens folder is the remote one.
    */
   public function updateRemoteScreens() {
-    $cache_directory = self::CACHE_DIR;
+    $cache_directory = static::CACHE_DIR;
+
     if (!file_prepare_directory($cache_directory, FILE_CREATE_DIRECTORY | FILE_MODIFY_PERMISSIONS)) {
-      $this->logger->error("Failed to create screen cache directory: @directory", ['@directory' => $cache_directory]);
+      $this->logger->error('Failed to create screen cache directory: @directory', [
+        '@directory' => $cache_directory,
+      ]);
+
       return;
     }
-    $path_template = '%s/%s.%s';
-    foreach (self::ALLOWED_SCREENS as &$name) {
-      foreach (self::ALLOWED_TYPES as &$ext) {
-        $screen_source = sprintf($path_template, $this->path, $name, $ext);
-        $screen_destination = sprintf($path_template, $cache_directory, $name, $ext);
+
+    foreach (static::ALLOWED_SCREENS as $name) {
+      foreach (static::ALLOWED_TYPES as $type) {
+        $screen_source = static::buildPath($this->path, $name, $type);
+        $screen_destination = static::buildPath($cache_directory, $name, $type);
         $response = $this->httpClient->get($screen_source);
-        if ($response->getStatusCode() != 200) {
-          $this->logger->error("Error during retrieving janrain remote screen (@url) ", [
+
+        if ($response->getStatusCode() !== 200) {
+          $this->logger->error('Error during retrieving Janrain remote screen (@url)', [
             '@url' => $screen_source,
           ]);
+
           continue;
         }
-        $success = file_unmanaged_save_data($response->getBody(), $screen_destination, FILE_EXISTS_REPLACE);
-        if ($success === FALSE) {
-          $this->logger->error("Failed to write @screenDest", ['@screenDest' => $screen_destination]);
+
+        if (file_unmanaged_save_data($response->getBody(), $screen_destination, FILE_EXISTS_REPLACE) === FALSE) {
+          $this->logger->error('Failed to write @screenDest', [
+            '@screenDest' => $screen_destination,
+          ]);
         }
       }
     }
+  }
+
+  /**
+   * Returns the constructed path.
+   *
+   * @param string $path
+   *   The base path.
+   * @param string $name
+   *   The name of a file.
+   * @param string $type
+   *   The extension of a file.
+   *
+   * @return string
+   *   The constructed path.
+   */
+  protected static function buildPath($path, $name, $type) {
+    return sprintf('%s/%s.%s', $path, $name, $type);
   }
 
 }

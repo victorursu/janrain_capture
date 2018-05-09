b3e4647a9f61151e64a156b5cb17506858d46583
Sergii Bondarenko <broken@firstvector.org>
2018-05-09 11:24:45 +0300

    Implement the "janrainCaptureLoaded" event to be sure the Janrain Capture system is loaded

--- janrain-init.js
+++ janrain-init.js
@@ -1,53 +1,67 @@
-if (typeof window.janrain !== 'object') window.janrain = {};
-Drupal.settings = {};
-Drupal.settings.basePath = drupalSettings.path.baseUrl;
-Drupal.settings.pathPrefix = drupalSettings.path.pathPrefix;
-Drupal.settings.janrainCapture = {};
-Drupal.settings.janrainCapture.sso_address = drupalSettings.janrain.capture.federate_server;
+window.janrain = window.janrain || {};
 
-window.janrain.settings = {};
-window.janrain.settings.capture = {};
+(function (janrain) {
+  'use strict';
 
-janrain.settings.appUrl = drupalSettings.janrain.app_url;
-janrain.settings.capture.captureServer = drupalSettings.janrain.capture.capture_server;
-janrain.settings.capture.appId = drupalSettings.janrain.capture.app_id;
-janrain.settings.capture.clientId = drupalSettings.janrain.capture.client_id;
+  janrain.settings = {};
+  janrain.settings.capture = {};
 
-// --- Engage Widget Settings ----------------------------------------------
-janrain.settings.language = drupalSettings.path.currentLanguage;
-janrain.settings.tokenUrl = janrain.settings.capture.captureServer;
+  janrain.settings.appUrl = drupalSettings.janrain.app_url;
+  janrain.settings.capture.captureServer = drupalSettings.janrain.capture.capture_server;
+  janrain.settings.capture.appId = drupalSettings.janrain.capture.app_id;
+  janrain.settings.capture.clientId = drupalSettings.janrain.capture.client_id;
 
-// --- Capture Widget Settings ---------------------------------------------
-janrain.settings.capture.language = drupalSettings.path.currentLanguage;
-janrain.settings.capture.redirectUri = drupalSettings.janrain.capture.redirect_uri;
-janrain.settings.capture.setProfileCookie = true;
-janrain.settings.capture.keepProfileCookieAfterLogout = true;
-janrain.settings.capture.responseType = 'code';
-janrain.settings.capture.stylesheets = drupalSettings.janrain.capture.stylesheets;
+  // --- Engage Widget Settings ----------------------------------------------
+  janrain.settings.language = drupalSettings.path.currentLanguage;
+  janrain.settings.tokenUrl = janrain.settings.capture.captureServer;
 
-// --- Federate ------------------------------------------------------------
-if (drupalSettings.janrain.capture.enable_sso) {
-  janrain.settings.capture.federate = true;
-  janrain.settings.capture.federateServer = drupalSettings.janrain.capture.federate_server;
-  janrain.settings.capture.federateXdReceiver = drupalSettings.janrain.capture.federate_xd_reciever;
-  janrain.settings.capture.federateLogoutUri = drupalSettings.janrain.capture.federate_logout_uri;
-  janrain.settings.capture.federateEnableSafari = true;
-}
+  // --- Capture Widget Settings ---------------------------------------------
+  janrain.settings.capture.language = drupalSettings.path.currentLanguage;
+  janrain.settings.capture.redirectUri = drupalSettings.janrain.capture.redirect_uri;
+  janrain.settings.capture.setProfileCookie = true;
+  janrain.settings.capture.keepProfileCookieAfterLogout = true;
+  janrain.settings.capture.responseType = 'code';
+  janrain.settings.capture.stylesheets = drupalSettings.janrain.capture.stylesheets;
 
-function isReady() {
-  janrain.ready = true;
-};
-if (document.addEventListener) {
-  document.addEventListener("DOMContentLoaded", isReady, false);
-} else {
-  window.attachEvent('onload', isReady);
-}
+  // --- Federate ------------------------------------------------------------
+  if (drupalSettings.janrain.capture.enable_sso) {
+    janrain.settings.capture.federate = true;
+    janrain.settings.capture.federateServer = drupalSettings.janrain.capture.federate_server;
+    janrain.settings.capture.federateXdReceiver = drupalSettings.janrain.capture.federate_xd_reciever;
+    janrain.settings.capture.federateLogoutUri = drupalSettings.janrain.capture.federate_logout_uri;
+    janrain.settings.capture.federateEnableSafari = true;
+  }
 
-var e = document.createElement('script');
-e.type = 'text/javascript';
-e.id = 'janrainAuthWidget';
-var url = document.location.protocol === 'https:' ? 'https://' : 'http://';
-url += drupalSettings.janrain.capture.load_js_url;
-e.src = url;
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(e, s);
+  Drupal.settings = {};
+  Drupal.settings.basePath = drupalSettings.path.baseUrl;
+  Drupal.settings.pathPrefix = drupalSettings.path.pathPrefix;
+  Drupal.settings.janrainCapture = {};
+  Drupal.settings.janrainCapture.sso_address = drupalSettings.janrain.capture.federate_server;
+
+  var script = document.createElement('script');
+  var event = document.createEvent('Event');
+  var onload = function () {
+    janrain.ready = true;
+  };
+
+  event.initEvent('janrainCaptureLoaded', true, true);
+
+  script.id = 'janrainAuthWidget';
+  script.src = document.location.protocol + '//' + drupalSettings.janrain.capture.load_js_url;
+
+  if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', onload, false);
+  }
+  else {
+    window.attachEvent('onload', onload);
+  }
+
+  var interval = setInterval(function() {
+    if (janrain.hasOwnProperty('capture') && typeof janrain.capture.ui === 'object') {
+      clearInterval(interval);
+      document.dispatchEvent(event);
+    }
+  }, 500);
+
+  document.head.appendChild(script);
+})(window.janrain);

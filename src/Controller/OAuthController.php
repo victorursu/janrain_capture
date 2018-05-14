1f5bc669f946e641a477f6ca18a7365a68dd1656
Sergii Bondarenko <broken@firstvector.org>
2018-05-14 13:01:57 +0300

    Redirect to the page in the "destination" GET parameter after login

--- src/Controller/OAuthController.php
+++ src/Controller/OAuthController.php
@@ -3,9 +3,9 @@
 namespace Drupal\janrain_capture\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Url;
 use Drupal\janrain_capture\JanrainCaptureApiService;
-use Drupal\user\Entity\User;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,56 +20,75 @@ class OAuthController extends ControllerBase {
    *
    * @var \Drupal\janrain_capture\JanrainCaptureApiService
    */
-  private $apiService;
+  protected $apiService;
+  /**
+   * The storage of the "user" entities.
+   *
+   * @var \Drupal\user\UserStorageInterface
+   */
+  protected $userStorage;
 
   /**
-   * OAuthController constructor.
-   *
-   * @param \Drupal\janrain_capture\JanrainCaptureApiService $api_service
-   *   Janrain Capture API Service.
+   * {@inheritdoc}
    */
-  public function __construct(JanrainCaptureApiService $api_service) {
+  public function __construct(
+    JanrainCaptureApiService $api_service,
+    EntityTypeManagerInterface $entity_type_manager
+  ) {
     $this->apiService = $api_service;
+    $this->userStorage = $entity_type_manager->getStorage('user');
   }
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static($container->get('janrain_capture.api_service'));
+    return new static(
+      $container->get('janrain_capture.api_service'),
+      $container->get('entity_type.manager')
+    );
   }
 
   /**
    * Login user to the system using oauth code passed in request.
    *
    * @param \Symfony\Component\HttpFoundation\Request $request
-   *   Incoming HTTP request.
+   *   The incoming HTTP request.
    *
    * @return \Symfony\Component\HttpFoundation\Response
-   *   Url to frontpage.
+   *   The URL to a front page or to a page in the "destination" GET parameter.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function login(Request $request) {
-    $oauth_url = Url::fromRoute('janrain_capture.oauth', [], ['absolute' => TRUE])
-      ->toString();
-    $frontpage_url = Url::fromRoute('<front>', [], ['absolute' => TRUE])
-      ->toString();
-    $code = $request->query->get('code');
-    $data = $this->apiService->newAccessToken($code, $oauth_url);
+    $destination = $request->query->has('destination')
+      ? Url::fromUserInput($request->query->get('destination'))
+      : Url::fromRoute('<front>');
+
+    $data = $this->apiService->newAccessToken(
+      $request->query->get('code'),
+      Url::fromRoute('janrain_capture.oauth')->setAbsolute()->toString()
+    );
+
     $user_entity = $this->apiService->getUserEntity();
     $email = $user_entity->result->email;
+    /* @var \Drupal\user\UserInterface $account */
     $account = user_load_by_mail($email);
-    if (!$account) {
-      $account = User::create([
+
+    if ($account === FALSE) {
+      $account = $this->userStorage->create([
         'name' => $user_entity->result->uuid,
         'mail' => $email,
         'status' => 1,
       ]);
-      $account->save();
+
+      $this->userStorage->save($account);
     }
+
     user_login_finalize($account);
     $this->apiService->updateCaptureSession($data);
 
-    return new Response($frontpage_url);
+    return new Response($destination->setAbsolute()->toString());
   }
 
 }
